fdec0b807c9ae6942d0efba563c129f846549bd0
shlomiha <shlomiha@radware.com>
2025-01-24 14:27:41 +0200

    added unittests

--- python_library/__init__.py
+++ python_library/__init__.py
@@ -1,4 +1,4 @@
-from .python_methods import add_record, retrieve_record, download_db
+from .s3_operations import add_record, retrieve_record, download_db
 from .credentials import load_credentials
 
 __all__ = ["add_record", "retrieve_record", "download_db", "load_credentials"]
